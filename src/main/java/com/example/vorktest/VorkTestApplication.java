7511195cd77a56f2db876c61607716594fffd6fc
シム ウク <astralkaiser89@gmail.com>
2024-03-01 18:10:42 +0900

    Initial commit

---
+++ src/main/java/com/example/vorktest/VorkTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.vorktest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VorkTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VorkTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/vorktest/VorkTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vorktest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VorkTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
